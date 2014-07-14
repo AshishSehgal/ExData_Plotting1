051170e20a43da64a5e8cbad7ffe7c35e2192c5d
Ashish Sehgal <ash023seh@gmail.com>
2014-07-14 14:47:03 +0530

    Done

---
+++ Plot2.R
@@ -0,0 +1,10 @@
+source("readingdata.R")
+
+Plot2 <- function() {
+    tbl <- readingdata()
+    png(filename = "plot2.png", width = 480, height = 480, units = "px")
+    plot(tbl$DateTime, tbl$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+    dev.off()
+}
+
+Plot2()
---
+++ readingdata.R
@@ -0,0 +1,5 @@
+readingdata<-function(){
+	tbl <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+      tbl <- tbl[(tbl$Date == "1/2/2007") | (tbl$Date == "2/2/2007"),]
+tbl
+}
