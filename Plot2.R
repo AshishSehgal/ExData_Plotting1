aa8ba9003f785719572d9afda7bdca23bc7b329d
Ashish Sehgal <ash023seh@gmail.com>
2014-08-10 22:38:37 +0530

    commit

--- Plot2.R
+++ Plot2.R
@@ -3,7 +3,8 @@ source("readingdata.R")
 Plot2 <- function() {
     tbl <- readingdata()
     png(filename = "plot2.png", width = 480, height = 480, units = "px")
-    plot(tbl$DateTime, tbl$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+    tbl$Date <- strptime(paste(tbl$Date,tbl$Time), "%d/%m/%Y %H:%M:%S")
+    plot(tbl$Date, tbl$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
     dev.off()
 }
 
